0ab8a26fa737332a60518bf8ec2efa2559434c43
wh2887 <13567272790@163.com>
2020-06-19 20:51:03 +0800

    modify parcel url

--- dist/main.45c6cd9e.js
+++ dist/main.45c6cd9e.js
@@ -19,4 +19,4 @@ var e,t=arguments[3],n=require("process");!function(e,t){"use strict";"object"==
 },{"./app4.css":"1AQo","jquery":"juYr"}],"epB2":[function(require,module,exports) {
 "use strict";require("./reset.css"),require("./global.css"),require("./app1.js"),require("./app2.js"),require("./app3.js"),require("./app4.js");
 },{"./reset.css":"1AQo","./global.css":"1AQo","./app1.js":"U+s5","./app2.js":"vZ5o","./app3.js":"y8lT","./app4.js":"eWpN"}]},{},["epB2"], null)
-//# sourceMappingURL=main.45c6cd9e.js.map
+//# sourceMappingURL=https://wh2887.github.io/MVC-1/dist/main.45c6cd9e.js.map

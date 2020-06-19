e7bb895f7f51fd5ceaff282f5244830920d7a413
wh2887 <13567272790@163.com>
2020-06-19 22:13:04 +0800

    app3-bug

--- src/app2.js
+++ src/app2.js
@@ -3,14 +3,18 @@ import $ from 'jquery'
 
 const $tabBar = $('.app2 .tab-bar')
 const $tabContent = $('.app2 .tab-content')
+const localKey = 'app2.index'
+//  保底值
+const index = localStorage.getItem(localKey) ?? 0
 $tabBar.on('click', 'li', e => {
   const $li = $(e.currentTarget)
   $li.addClass('selected')
     .siblings().removeClass('selected')
   let index = $li.index()
+  localStorage.setItem(localKey,index)
   // 样式与行为分离
   $tabContent.children().eq(index).addClass('active')
     .siblings().removeClass('active')
 })
 // 默认哪个被选中
-$tabBar.children().eq(0).trigger('click')
+$tabBar.children().eq(index).trigger('click')
--- src/app3.js
+++ src/app3.js
@@ -1,8 +1,23 @@
-import './app3.css'
 import $ from 'jquery'
+import './app3.css'
 
 const  $square = $('.app3 .square')
+const localKey = 'app3.active'
+const active = localStorage.getItem(localKey) === 'yes'
+if (active){
+  $square.addClass('active')
+}else {
+  $square.removeClass('active')
+}
+// $square.toggleClass('active',active)
 $square.on('click',()=>{
   //如果没有这个样式类，就加上，如果有就去掉这个样式
-  $square.toggleClass('active')
+  // $square.toggleClass('active')
+  if($square.hasClass('active')){
+    $square.removeClass('active')
+    localStorage.setItem(localKey,'no')
+  }else {
+    $square.addClass('active')
+    localStorage.setItem(localKey,'yes')
+  }
 })
